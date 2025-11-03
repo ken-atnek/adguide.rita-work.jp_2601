4d9819e7c987c5895df49b6dd10ab0b3b23acde7
Kenta Urakami <ken.atnek@gmail.com>
2025-11-03 16:42:26 +0900

    レスポンシブ調整途中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
     <>
       <ContainerHeader />
       <ContainerFocusList />
-      <ContainerStructure />ｔ
+      <ContainerStructure />
       <ContainerResponse />
       <ContainerContact />
     </>
--- src/components/Top/ContainerHeader.tsx
+++ src/components/Top/ContainerHeader.tsx
@@ -6,8 +6,8 @@
  * Last updated: 2025-10-01
  * ======================================= */
 'use client';
-import { useScrollTrigger } from '@/hooks/useScrollTrigger';
 import styles from '@/styles/TopHero.module.scss';
+import { useScrollTrigger } from '@/hooks/useScrollTrigger';
 import { SHOP_INFO } from '@/config/shop';
 
 const ContainerHeader = () => {
@@ -18,7 +18,13 @@ const ContainerHeader = () => {
       ref={ref}
     >
       <div className={styles.boxH1}>
-        <span>求人掲載をご検討の事業者様へ</span>
+        <span>
+          求人掲載を
+          <br className="sp" />
+          ご検討の
+          <br className="sp" />
+          事業者様へ
+        </span>
         <h1>
           熊本「医療＆介護」特化型求人サイト
           <svg aria-label={SHOP_INFO.title}>
@@ -33,7 +39,10 @@ const ContainerHeader = () => {
           <span className={styles.textBottom}>
             <i>勝</i>
             <i>ち</i>
-            <i>筋</i>がある
+            <i>筋</i>
+            が
+            <br className="sp" />
+            ある
           </span>
         </p>
         <div className={styles.svgText}>
@@ -51,14 +60,18 @@ const ContainerHeader = () => {
         <article>
           <div className={styles.text}>
             RITA
-            <br />
+            <br className="pc" />
             WORK
-            <br />
+            <br className="pc" />
+            <i />
             is...
           </div>
           <div className={styles.wrapList}>
             <h2>
-              その布石は、<i>4</i>つの特化から。
+              その布石は、 <br className="sp" />
+              <i>4</i>つの
+              <br className="sp" />
+              特化から。
             </h2>
             <nav>
               <a href="#">熊本県の「医療&介護」特化型転職サイト</a>
